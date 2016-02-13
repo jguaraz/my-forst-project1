3b1110cc25a354e4065299b28aba5bd899d15bb2
jguaraz <jguaraz@openmailbox.org>
2016-02-13 16:01:43 -0300

    Primer proyecto django

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/juangu/lab/python/django/projects/djangogirls/myenv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
